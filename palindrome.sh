0f2d3324ceba13f1a45a0ab1a85a06dcc4e9b1fb
Jerintom-10 <jerintom468@gmail.com>
2025-04-24 15:36:21 +0530

    Update palindrome.sh

--- palindrome.sh
+++ palindrome.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# Prompt the user for the string to check
 echo "Enter a string: "
 read string
-# Reverse the string
 reverse=$(echo $string | rev)
-# Check if the string is equal to its reverse
 if [ "$string" == "$reverse" ]; then
 echo "$string is a palindrome."
 else
