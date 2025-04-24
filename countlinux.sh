e007e352011836356dbb480c9fb162c32d6ff3cb
Jerintom-10 <jerintom468@gmail.com>
2025-04-24 15:34:02 +0530

    Update countlinux.sh

--- countlinux.sh
+++ countlinux.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-# Set the search directory
 search_dir="."
-# Find all ".txt" files in the search directory and its subdirectories
 files=$(find "$search_dir" -type f -name "*.txt")
-# Initialize the count
 count=0
-# Loop through each file and count the occurrences of "Linux"
 for file in $files; do
 occurrences=$(grep -o "Linux" "$file" | wc -l)
 count=$((count + occurrences))
